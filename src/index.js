3adb5eda05e9bb96f852cf522e958547f7ee126e
SkakalaJ <juraj.skakala1@gmail.com>
2024-06-26 09:44:29 +0200

    sadasd

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 // Redux
 import { Provider } from 'react-redux';
-import store from './app/store';
+import store from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
